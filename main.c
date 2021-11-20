6f5bd14b981c77830ef483acd6b1c217cd021003
jonesluke1999 <59861784+jonesluke1999@users.noreply.github.com>
2021-11-19 19:28:45 -0500

    Update main.c
    added adsr, pulse width modulation, changed InitAudioStream to LoadAudioStream

--- main.c
+++ main.c
@@ -10,14 +10,30 @@
 #define SLIDER_HEIGHT 150
 #define SLIDER_WIDTH 30
 #define SLIDER_VISIBLE_WIDTH 10
+#define MAX_ATTACK_TIME 3
+#define MAX_DECAY_TIME 5
+#define NUM_SLIDERS 8
 
 typedef struct{
     float phase;
     float phaseStride;
+    float frequency;
     float threshold;
     float octave;
     float octaveFactor;
+    float PWM_phase;
+    float PWM_phaseStride;
+    float PWM_frequency;
+    float PWM_val;
 } Oscillator;
+typedef struct{
+    float attack;
+    float decay;
+    float sustain;
+    float release;
+    float amplitude;
+    bool decayPhase;
+} ADSR_Control;
 typedef struct{
     bool black;
     float frequency;
@@ -38,16 +54,36 @@ typedef struct{
 } Input;
 
 Key keys[17];
-Slider sliders[2];
+Slider sliders[8];
 Oscillator osc;
+ADSR_Control adsr;
 Input masterInput;
 float buffer[1024];
-float frequency = 10;
-float threshold = 0;
-float amplitude = 0;
 float sample_duration;
-
-void updateSignal(float* signal, float frequency, float sample_duration, float amplitude){
+float calculateAmp(){
+    if(masterInput.keyPressed){
+        if(!adsr.decayPhase){
+            adsr.amplitude += 1.0/(SAMPLE_RATE*MAX_ATTACK_TIME*(adsr.attack+0.001));
+            if(adsr.amplitude > 0.99){
+                adsr.amplitude = 1;
+                adsr.decayPhase = true;
+            }
+        }
+        else{
+            if(adsr.amplitude > adsr.sustain){
+                adsr.amplitude -= (1.0-adsr.sustain)/(SAMPLE_RATE*MAX_ATTACK_TIME*(adsr.decay+0.001));
+            }
+        }
+    }
+    else{
+        adsr.decayPhase = false;
+        if(adsr.amplitude > 0){
+            adsr.amplitude -= 1.0/(SAMPLE_RATE*MAX_ATTACK_TIME*(adsr.release+0.001));
+        }
+    }
+    return adsr.amplitude;
+}
+void updateSignal(float* signal, float sample_duration){
     if(osc.octave < 0.25){
         osc.octaveFactor = 0.25;
     }
@@ -60,18 +96,24 @@ void updateSignal(float* signal, float frequency, float sample_duration, float a
     else{
         osc.octaveFactor = 2;
     }
-    osc.phaseStride = frequency * sample_duration * osc.octaveFactor;
+    osc.PWM_phaseStride = osc.PWM_frequency*20 * sample_duration;
+    osc.phaseStride = osc.frequency * sample_duration * osc.octaveFactor;
     for(int i = 0; i < 1024; i++){
         osc.phase += osc.phaseStride;
         if(osc.phase > 1) osc.phase -= 1;
         float sin_value = sinf(2.0f * PI * osc.phase);
-        if(sin_value > osc.threshold){
+        //PWM phase
+        osc.PWM_phase += osc.PWM_phaseStride;
+        if(osc.PWM_phase > 1) osc.PWM_phase -= 1;
+        float PWM_sin_value = sinf(2.0f * PI * osc.PWM_phase)*osc.PWM_val;
+        float threshold = 0.5*(osc.threshold + PWM_sin_value);
+        if(sin_value > threshold){
             signal[i] = 0.5;
         }
         else {
             signal[i] = -0.5;
         }
-        signal[i] *= amplitude;
+        signal[i] *= calculateAmp();
     }
 }
 void drawWaveform(float* signal,int width,int height,int x, int y){
@@ -84,7 +126,7 @@ void drawWaveform(float* signal,int width,int height,int x, int y){
     prev.x = x;
     prev.y = (height/2)+0.5*(int)(signal[0]*100)+y;
     for(int i = 1; i < width - 1; i++){
-        int index = (start + (int)(500*i/(frequency*osc.octaveFactor))%loop)%STREAM_BUFFER_SIZE;
+        int index = (start + (int)(500*i/(osc.frequency*osc.octaveFactor))%loop)%STREAM_BUFFER_SIZE;
         Vector2 current;
         current.x = i+x;
         current.y = (height/2)+0.5*(int)(signal[index]*100)+y;
@@ -121,24 +163,67 @@ void buildKeys(){
     }
 }
 void buildSliders(){
-    Slider threshold;
-    threshold.xPos = 300;
-    threshold.yPos = 100;
-    threshold.value = 0;
-    threshold.param = &osc.threshold;
-    threshold.name = "PULSE WIDTH";
-    sliders[0] = threshold;
     Slider octave;
     octave.xPos = 200;
     octave.yPos = 100;
     octave.value = 0;
     octave.param = &osc.octave;
     octave.name = "OCTAVE";
-    sliders[1] = octave;
+    sliders[0] = octave;
+    Slider threshold;
+    threshold.xPos = 300;
+    threshold.yPos = 100;
+    threshold.value = 0;
+    threshold.param = &osc.threshold;
+    threshold.name = "PULSE WIDTH";
+    sliders[1] = threshold;
+    Slider PWM_freq;
+    PWM_freq.xPos = 450;
+    PWM_freq.yPos = 100;
+    PWM_freq.value = 0;
+    PWM_freq.param = &osc.PWM_frequency;
+    PWM_freq.name = "PWM Freq";
+    sliders[2] = PWM_freq;
+    Slider PWM_val;
+    PWM_val.xPos = 600;
+    PWM_val.yPos = 100;
+    PWM_val.value = 0;
+    PWM_val.param = &osc.PWM_val;
+    PWM_val.name = "PWM Val";
+    sliders[3] = PWM_val;
+    Slider Attack;
+    Attack.xPos = 200;
+    Attack.yPos = 350;
+    Attack.value = 0;
+    Attack.param = &adsr.attack;
+    Attack.name = "Attack";
+    sliders[4] = Attack;
+    Slider Decay;
+    Decay.xPos = 300;
+    Decay.yPos = 350;
+    Decay.value = 0;
+    Decay.param = &adsr.decay;
+    Decay.name = "Decay";
+    sliders[5] = Decay;
+    Slider Sustain;
+    Sustain.xPos = 450;
+    Sustain.yPos = 350;
+    Sustain.value = 0;
+    Sustain.param = &adsr.sustain;
+    Sustain.name = "Sustain";
+    sliders[6] = Sustain;
+    Slider Release;
+    Release.xPos = 600;
+    Release.yPos = 350;
+    Release.value = 0;
+    Release.param = &adsr.release;
+    Release.name = "Release";
+    sliders[7] = Release;
+    
     
 }
 void drawSliders(){
-    for(int i = 0; i < 2; i++){
+    for(int i = 0; i < 8; i++){
         Slider tempSlider = sliders[i];
         //draw black rectangle
         int visibleXPos = tempSlider.xPos + SLIDER_WIDTH/2 - SLIDER_VISIBLE_WIDTH/2;
@@ -203,7 +288,6 @@ void processInput(){
     if(IsMouseButtonDown(0)){
         if(masterInput.y > (3*SCREEN_HEIGHT / 4)){
             masterInput.keyPressed = true;
-            amplitude = 1;
             bool checkBlack = false;
             bool foundKey = false;
             int keyIndex = -1;
@@ -236,13 +320,13 @@ void processInput(){
                     }
                 }
             }
-            frequency = keys[keyIndex].frequency;
+            osc.frequency = keys[keyIndex].frequency;
             keys[keyIndex].pressed = true;
         }
         else{
             clearKeyPress();
             //check if slider is selected
-            for(int i = 0; i < 2; i++){
+            for(int i = 0; i < 8; i++){
                 Slider tempSlider = sliders[i];
                 if(masterInput.x > tempSlider.xPos && masterInput.x -tempSlider.xPos < SLIDER_WIDTH && masterInput.y > tempSlider.yPos && masterInput.y -tempSlider.yPos < SLIDER_HEIGHT){
                     tempSlider.value = (float)(tempSlider.yPos + SLIDER_HEIGHT - masterInput.y)/SLIDER_HEIGHT;
@@ -253,24 +337,37 @@ void processInput(){
         }
     }
     else {
-        amplitude = 0;
         clearKeyPress();
     }
+}
+void initOscADSR(){
+    osc.frequency = 200;
+    osc.threshold = 0;
+    osc.octave = 0;
+    osc.PWM_phase = 0;
+    osc.PWM_phaseStride = 0;
+    osc.PWM_frequency = 0;
+    osc.PWM_val = 0;
+    sample_duration = (1.0f)/SAMPLE_RATE;
+    osc.phase = 0;
+    osc.phaseStride = osc.frequency * sample_duration;
+    osc.phase = 0;
+    osc.phaseStride = osc.frequency * sample_duration;
+    adsr.amplitude = 0;
+    adsr.attack = 0;
+    adsr.decay = 0;
+    adsr.release = 0;
+    adsr.sustain = 0;
 }
 void main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Synth");
     SetTargetFPS(60);
     InitAudioDevice();
-    frequency = 200;
-    osc.threshold = 0;
-    osc.octave = 0;
+    initOscADSR();
     buildKeys();
     buildSliders();
-    sample_duration = (1.0f)/SAMPLE_RATE;
-    osc.phase = 0;
-    osc.phaseStride = frequency * sample_duration;
     SetAudioStreamBufferSizeDefault(1024);
-    AudioStream synthStream = InitAudioStream(SAMPLE_RATE,
+    AudioStream synthStream = LoadAudioStream(SAMPLE_RATE,
         32 ,
         1
     );
@@ -281,7 +378,7 @@ void main() {
         if(IsAudioStreamProcessed(synthStream)){
             UpdateAudioStream(synthStream, buffer, STREAM_BUFFER_SIZE);
             processInput();
-            updateSignal(buffer, frequency, sample_duration, amplitude);
+            updateSignal(buffer, sample_duration);
             drawGUI();
         }
     }
